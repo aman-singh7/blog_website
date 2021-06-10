3686a84fd50df6abd1d3bec918a7264dd562dbe7
aman-singh7 <aman2@me.iitr.ac.in>
2021-06-10 18:39:24 +0530

    implement register user

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog-Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog-Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018',
-    },
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home',
     }
     return render(request, 'blog/home.html',context)
