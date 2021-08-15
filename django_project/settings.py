8a0ab08835eb4bd4de975b8c70746f0f1456334f
aman-singh7 <aman2@me.iitr.ac.in>
2021-08-16 00:48:25 +0530

    feat: add gitignore and procfile

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vlcuhks0#-#qee0dm5@&^-y8wpat&(x(&@67w9%_i4h6v&n4im'
+# SECRET_KEY = 'django-insecure-vlcuhks0#-#qee0dm5@&^-y8wpat&(x(&@67w9%_i4h6v&n4im'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myawesomedblogapp.herokuapp.com']
 
 
 # Application definition
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL= '/media/'
